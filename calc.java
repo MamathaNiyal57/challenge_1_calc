60d3d3e763bcfbb2480af17896158aee4388f242
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-26 18:50:19 +0530

    created a feature/addition branch

--- calc.java
+++ calc.java
@@ -7,8 +7,15 @@ public class calc
     public static void main(String args[])
     {
         Scanner sc =new Scanner(System.in);
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int height = sc.nextInt();
+    }
+
+    public static int addition(int a,int b)
+    {
+        int sum = a + b ;
+        return sum;
     }
 }
