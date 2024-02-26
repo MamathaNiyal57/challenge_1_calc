81e42e827b8034f2d6855d2d245c8c3e646e9af7
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-26 18:46:27 +0530

    written base code in calc file

--- calc.java
+++ calc.java
@@ -1,1 +1,14 @@
-#Assignment_1
+
+import java.util.*;
+//import java.io;
+
+public class calc
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int height = sc.nextInt();
+    }
+}
